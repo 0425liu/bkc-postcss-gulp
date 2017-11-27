6d312ee35ce1baec1c1e4f9af860c0f2667e7af2
liukang <7970223@qq.com>
2017-11-27 14:28:52 +0800

    a.CSS

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+gulp.task('css', function() {
+    var plugins = [
+         require('postcss-import')(),
+        require('postcss-mixins')(),
+        require('postcss-for')(),
+        require('postcss-simple-vars')({silent: true}}), 
+        require('postcss-nested')(),
+        require('postcss-extend-rule')(),
+        cssnext({
+            autoprefixer: {
+                browsers: ['IE >= 9']
+            }
+        }),
+
+
+    ];
+    return gulp.src('src/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('auto_postcss',function(){
+    gulp.watch('src/*.css',['css']);
+}) 
